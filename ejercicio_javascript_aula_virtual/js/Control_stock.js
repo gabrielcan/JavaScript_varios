d655ca22951aee3f1c7e7cf84ac62681a5bcba83
Gabriel Cano <gabrielcano.86@gmail.com>
2020-01-05 18:54:28 -0300

    actualizo ejercicio conrol stock

--- ejercicio_javascript_aula_virtual/js/Control_stock.js
+++ ejercicio_javascript_aula_virtual/js/Control_stock.js
@@ -16,58 +16,118 @@ Los métodos para estas categorías de producto serán
 ·       Consultar precio de un producto determinado*/
 
 class Producto{
-    constructor(){
-        this.nombre;
-        this.precio;
-        this.cantidad; // el stock se calcula
-        this.categoria;
+    constructor(nombre,precio,cat){
+        this.nombre=nombre;
+        this.precio=precio;
+        //this.cantidad=1; // el stock se calcula
+        this.categoria=cat;
     }
     
-agregarProductos(){
+}
 
-   let nombreP= prompt('Ingrese Nombre de producto: ');
-   let valorP= prompt('Ingrese Precio del producto: ');
-   let cantP= prompt('Ingrese cantidad del producto: ');
-   let categP= prompt('Ingrese grupo nuemero del grupo del producto (0: Comestibles y 1: Bebidas): ');
+class Gestion{
 
-    this.nombre=nombreP;
-    this.precio=valorP;
-    this.cantidad=cantP; // el stock se calcula
-    this.categoria=categP;
+    constructor(){
+        this.listarProductos=[];
+    }
+
+    agregarProductos(productNuevo){
+
+    const productoEnLista = this.listarProductos.find(function(producto) {
+            return productNuevo.nombre == producto.nombre;
+          })
+           
+          if (productoEnLista) {
+              productoEnLista.cantidad++;
+          } else {
+            this.listarProductos.push(productNuevo);
+          }
+        }
+
+    
+    
+    
+    
+    cambiarCantidadstock(){
+    
+    }
+    
+    cambiarPrecio(){
+    
+    }
+    
+    mostrarProductos(){
+
+        for(let i=0;i<this.listarProductos.length;i++){
+
+        console.log(this.listarProductos[i])
+        }
+ 
+    }
+    
+    consultarStockProducto(nombre){
+    
+    }
+    
+    consultarPrecioProducto(prodBuscado){
+    
+        const precioProduct = this.listarProductos.find(function(producto) {
+            return prodBuscado==producto.nombre;
+          })    
+
+          if(precioProduct){
+              console.log('el precio del producto ',precioProduct.nombre,'es: ',precioProduct.precio)
+          } else {
+            console.log('No hay coincidencias del producto buscado');
+          }
+    }
+}
+
+ let cant= prompt('ingrese cantidad de productos a crear')
+
+ const agregar=new Gestion();
+
+ for (let i = 0; i < cant; i++) {
+         
+let nombreP= prompt('Ingrese Nombre de producto: ');
+let valorP= prompt('Ingrese Precio del producto: ');
+let categP= prompt('Ingrese numero del grupo del producto (0: Comestibles y 1: Bebidas): ');
+if(categP==0){
+    categP='bebida';
+} else {
+    categP='comestible';
+}
+const producto1=new Producto(nombreP,valorP,categP);
+
+
+agregar.agregarProductos(producto1);
+
+//  console.log(producto1);
 
 }
 
+const categoria = ['bebida','comestible'];
+const coca = new Producto('coca',10,categoria[0]);    
+const birra = new Producto('andes',11,categoria[0]);    
+const pan = new Producto('bimbo',10,categoria[1]);    
+const galleta = new Producto('pepa',10,categoria[1]);
 
-cambiarCantidadstock(){
+agregar.agregarProductos(birra);
+agregar.agregarProductos(coca);
+agregar.agregarProductos(pan);
+// agregar.agregarProductos(birra);
 
-}
 
-cambiarPrecio(){
+agregar.mostrarProductos();
 
-}
+let buscarPrecio=prompt('ingrese el nombre del producto que quiere saber el precio');
 
-listarProductos(){
+agregar.consultarPrecioProducto(buscarPrecio);
 
-}
+// const mostrar= new Gestion();
 
-consultarStockProducto(nombre){
+// mostrar.mostrarProductos();
 
-}
-
-consultarPrecioProducto(nombre){
-
-}
-
-}
-
-const product1= new Producto();
-let arrayProducto=[].push(new Producto('cerveza','20','bebible'));
-product1.agregarProductos();
-console.log(product1);
-product1.agregarProductos();
-console.log(product1);
-arrayProducto=[].push(product1);
-console.log(arrayProducto);
 
 
 
